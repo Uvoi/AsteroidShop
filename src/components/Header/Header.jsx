b3caebe3520e223233c45f5878d9c63284aabc93
Uvoi <iovu_dmtr@yahoo.com>
2024-06-10 00:48:45 +0300

    feat(components/Header) добавление смены цвета из-за темы и замена обычных ссылко в меню на StarButton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import './styles.css'
 import logo from './../../images/logo.svg'
-import user from './../../images/user.svg'
+import user from './../../images/userImg.png'
 import basket from './../../images/basket.svg'
+import StarButton from '../StarButton/StarButton';
 
 
 
 
-const Header = ({onToggleTheme})=>
+const Header = ({onToggleTheme, theme})=>
 {
 
     return(
-        <div id='Header'>
+        <div id='Header' style={{background: theme.palette.header.primary}}>
             <div id="header">
                 <div id="LogoH"><button onClick={onToggleTheme}><img src={logo} alt="" /></button></div>
                 <div id='MenuH' className='text_stroke'>
                     <ul>
-                        <li id='MainPageH'><a href='/'>Главная</a></li>
-                        <li id='CatalogPageH'><a href='/catalog'>Каталог</a></li>
-                        <li id='AboutPageH'><a href='/about'>О нас</a></li>
-                        <li id='ContactPageH'><a href='/help'>Помощь</a></li>
+                        <li ><StarButton href="/" theme={theme}>Главная</StarButton></li>
+                        <li ><StarButton href="/catalog" theme={theme}>Каталог</StarButton></li>
+                        <li ><StarButton href="/about" theme={theme}>О нас</StarButton></li>
+                        <li ><StarButton href="/help" theme={theme}>Помощь</StarButton></li>
+                        
                     </ul>
                 </div>
                 <div id="Basket_UserDataH">
-                    <a id='BasketH' href='/basket'><img src={basket} alt="" /></a>
+                    <a id='BasketH' href="/basket"><img src={basket} alt="" /></a>
                     <div id="UserDataH">
                         <a href='' className='bot_line'>baobab</a>
                         <a href=""><img src={user} alt="" /></a>
