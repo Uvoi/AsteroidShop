8e6706248bb08bd5a09a47b525c1c06ec0090033
Uvoi <iovu_dmtr@yahoo.com>
2024-06-19 15:23:39 +0300

    feat(backend/endpoints/data) добавление ендпоинта возвращающий массив продуктов по массиву id

--- backend/endpoints/data.py
+++ backend/endpoints/data.py
@@ -2,80 +2,6 @@ from fastapi import APIRouter, HTTPException, Response, Depends
 from data_base import getAllProducts, addNewProduct, addNewComment, getProductByID, getCompositionByID, getCommentsByProdID
 from pydantic import BaseModel
 
-result_all_json = [
-    {
-        "id": 1,
-        "title": "Астероид 1" ,
-        "description": "Ебейший астероид",
-        "weight": 200, 
-        "price": 15000,
-        "category": 'Железный'
-    },
-        {
-        "id": 2,
-        "title": "Астероид 1" ,
-        "description": "Ебейший астероид",
-        "weight": 200, 
-        "price": 15000,
-        "category": 'Железный'
-    },
-        {
-        "id": 21,
-        "title": "Астероид 1" ,
-        "description": "Ебейший астероид",
-        "weight": 200, 
-        "price": 15000,
-        "category": 'Железный'
-    },
-        {
-        "id": 3,
-        "title": "Астероид 1" ,
-        "description": "Ебейший астероид",
-        "weight": 200, 
-        "price": 15000,
-        "category": 'Железный'
-    },
-    {
-        "id": 4,
-        "title": "Астероид 1" ,
-        "description": "Ебейший астероид",
-        "weight": 200, 
-        "price": 15000,
-        "category": 'Железный'
-    },
-    {
-        "id": 5,
-        "title": "Астероид 1" ,
-        "description": "Ебейший астероид",
-        "weight": 200, 
-        "price": 15000,
-        "category": 'Железный'
-    },
-    {
-        "id": 6,
-        "title": "Астероид 1" ,
-        "description": "Ебейший астероид",
-        "weight": 200, 
-        "price": 15000,
-        "category": 'Железный'
-    },
-        {
-        "id": 7,
-        "title": "Астероид 1" ,
-        "description": "Ебейший астероид Ебейший астероид Ебейший астероид",
-        "weight": 200, 
-        "price": 15000,
-        "category": 'Железный'
-    },
-    {
-        "id": 8,
-        "title": "Астероид 1" ,
-        "description": "Ебейший астероид",
-        "weight": 200, 
-        "price": 15000,
-        "category": 'Железный'
-    },
-]
 
 
 router = APIRouter()
@@ -127,6 +53,18 @@ async def catalog_add(addNewRowData: addNewRowC):
 
 @router.post("/api/comments/add")
 async def comments_add(addNewCommentData: addNewCommentC):
-    # print(addNewCommentData)
+    #print(addNewCommentData)
     addNewComment(addNewCommentData.ProdID, addNewCommentData.UserID, addNewCommentData.Text)
     return addNewCommentData
+
+
+
+@router.post("/api/basket/getByMass")
+async def getProdsByMass(prod_mass: list[int]):
+    prod_mass.sort()
+    prods = []
+    for i in prod_mass:
+        prods.append(getProductByID(i))
+    # print(prods)
+    return prods
+    raise HTTPException(status_code=469, detail="Serv error")
