292c4baf813b4c3f63c0d633d450d73212c83afa
Uvoi <iovu_dmtr@yahoo.com>
2024-06-19 15:11:07 +0300

    feat(components/Basket BasketProduct) добавлена возможность удалять товары по отдельности и полностью + анимации

--- src/components/BasketProduct/BasketProduct.jsx
+++ src/components/BasketProduct/BasketProduct.jsx
@@ -1,50 +1,77 @@
 import React from 'react';
 import './styles.css';
+import { motion, useAnimation } from 'framer-motion';
+
 import pass from '../../images/pass.jpg';
 import { Button, Checkbox } from '@mui/material';
-import basket from './../../images/basket.svg';
-import { lightTheme, darkTheme } from '../../themes/theme';
-import { CheckBox } from '@mui/icons-material';
+import { darkTheme } from '../../themes/theme';
 
-const BasketProduct = ({ theme="", prdtTitle, prdtDescription, prdtDiameter, prdtWeight, prdtCategory="", prdtPrice, imgLink="", onRemove }) => {
-    const returnWeightCategory = (weight) => {
-        if ((weight > 0.000001) && (weight < 1)) 
-          return 'Малый'
-        if ((weight > 1) && (weight < 50))
-          return 'Средний'
-        if (weight > 50)
-          return 'Большой'
-      };
+const BasketProduct = ({ theme="", prdtTitle, prdtDescription, prdtDiameter, prdtWeight, prdtCategory="", prdtPrice, imgLink="", id, uniqueKey, deleteFunc=null }) => {
+  const returnWeightCategory = (weight) => {
+    if ((weight > 0.000001) && (weight < 1)) 
+      return 'Малый';
+    if ((weight > 1) && (weight < 50))
+      return 'Средний';
+    if (weight > 50)
+      return 'Большой';
+  };
 
-    return (
-        <div className='BasketProduct'>
-            <div style={{border: "1px solid"+theme.palette.primary.main}} className='basketProduct'>
-                <div className="imgDataBP">
-                    <img src={imgLink ? imgLink : pass} alt="" />
-                    <span style={prdtCategory == "Железный"?{color: "#A0A0A0"}:(prdtCategory == "Каменный"?{color: "#8f633f"}:{color:"#a18874"})}>{prdtCategory}</span>
-                </div>
-                <div className="prdtDataBP">
-                    <h3 style={{color: theme.palette.text.ultra}}>{prdtTitle}</h3>
-                    <p style={{color: theme.palette.text.secondary}}>{prdtDescription}</p>
-                    <div style={{color: theme.palette.text.primary}} className="prdtNumberDataBP">
-                        <p>
-                            <span>{returnWeightCategory(prdtWeight)}</span>
-                            <span><span>{prdtWeight}</span> тонн</span>
-                            <span><span>{prdtDiameter}</span> км.</span>
-                        </p>
-                    <div className="cost_buyBtnBP">
-                            <h2>{prdtPrice}.000 <sub>₽</sub></h2>
-                            <Checkbox className='checkBBP' defaultChecked/>
-                            <Button variant='contained' className='buyNowBP'>
-                                Купить
-                            </Button>
-                        </div>
-                    </div>
-                </div>
-                <button className="removeBtnBP" style={theme==darkTheme?{filter:`invert(100%)`}:{}} onClick={()=>console.log(theme==darkTheme)}/>
-            </div>
+  const controls = useAnimation();
+
+  const startAnimation = async () => {
+
+    if (Math.floor(Math.random() * 2) == 1)
+      {await controls.start({
+        x: 1900,
+        height: 0,
+        transition: { duration: 0.5 },
+      });}
+    else
+    {
+      {await controls.start({
+        x: -1900,
+        height: 0,
+        transition: { duration: 0.5 },
+      });}
+    }
+    deleteFunc(uniqueKey);
+  };
+
+  const deleteProduct = async () => {
+    await startAnimation();
+  };
+
+  return (
+    <motion.div className='BasketProduct' id={"BasketProductId="+uniqueKey}
+        animate={controls}
+    >
+      <div style={{border: "1px solid" + theme.palette.primary.main}} className='basketProduct'>
+        <div className="imgDataBP">
+          <img src={imgLink ? imgLink : pass} alt="" />
+          <span style={prdtCategory === "Железный" ? {color: "#A0A0A0"} : (prdtCategory === "Каменный" ? {color: "#8f633f"} : {color:"#a18874"})}>{prdtCategory}</span>
         </div>
-    );
+        <div className="prdtDataBP">
+          <h3 style={{color: theme.palette.text.ultra}}>{prdtTitle}</h3>
+          <p style={{color: theme.palette.text.secondary}}>{prdtDescription}</p>
+          <div style={{color: theme.palette.text.primary}} className="prdtNumberDataBP">
+            <p>
+              <span>{returnWeightCategory(prdtWeight)}</span>
+              <span><span>{prdtWeight}</span> тонн</span>
+              <span><span>{prdtDiameter}</span> км.</span>
+            </p>
+            <div className="cost_buyBtnBP">
+              <h2>{prdtPrice}.000 <sub>₽</sub></h2>
+              <Checkbox className='checkBBP' defaultChecked/>
+              <Button variant='contained' className='buyNowBP'>
+                Купить
+              </Button>
+            </div>
+          </div>
+        </div>
+        <button className="removeBtnBP" style={theme === darkTheme ? {filter: `invert(100%)`} : {}} onClick={deleteProduct}/>
+      </div>
+    </motion.div>
+  );
 };
 
 export default BasketProduct;
--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -1,23 +1,141 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './styles.css';
 import Button from '@mui/material/Button';
+import axios from 'axios';
+
 import { useNotification } from '../../components/Notification/Notification';
 import BasketProduct from '../../components/BasketProduct/BasketProduct';
-import pass from '../../images/pass.jpg'
+import { getProdsFromLS, delProdFromBasket, clearBasket } from '../../functions/user/user';
+import { motion, AnimatePresence, useAnimation } from 'framer-motion';
 
-const Basket = ({theme}) => {
-    const showNotification = useNotification();
+const Basket = ({ theme }) => {
+  const showNotification = useNotification();
+  const [prodData, setProdData] = useState([]);
+  const [isDeleteProd, setIsDeleteProd] = useState(false);
+  const [elementsVisible, setElementsVisible] = useState(false);
+  const initialLoad = useRef(true);
 
-    return (
-        <div id='Basket' style={{backgroundColor: theme.palette.background.paper}}>
-            <div id="basket">
-                <BasketProduct theme={theme} prdtDiameter={16.4} prdtTitle="testTitle" prdtDescription="dJHDsadajsda dadbkadba d kbad ka dJHDsadajsda dadbkadba d kbad ka dJHDsadajsda dadbkadba d kbad ka dJHDsadajsda dadbkadba d kbad ka dJHDsadajsda dadbkadba d kbad ka dJHDsadajsda dadbkadba d kbad ka dJHDsadajsda dadbkadba dJHDsadajsda dadbkadba dJHDsadajsda dadbkadba dJHDsadajsda dadbkadba d kbad ka dJHDsadajsda dadbkadba d kbad ka" prdtWeight={100} prdtCategory="Каменный" prdtPrice={500} imgLink={pass}/>   
-                <BasketProduct theme={theme} prdtDiameter={16.4} prdtTitle="testTitle" prdtDescription="dJHDsadajsda dadbkadba d kbad ka" prdtWeight={100} prdtCategory="Каменный" prdtPrice={500} imgLink={pass}/>   
-                <BasketProduct theme={theme} prdtDiameter={16.4} prdtTitle="testTitle" prdtDescription="dJHDsadajsda dadbkadba d kbad ka" prdtWeight={100} prdtCategory="Каменный" prdtPrice={500} imgLink={pass}/>   
-                <BasketProduct theme={theme} prdtDiameter={16.4} prdtTitle="testTitle" prdtDescription="dJHDsadajsda dadbkadba d kbad ka" prdtWeight={100} prdtCategory="Каменный" prdtPrice={500} imgLink={pass}/>   
-            </div>
-        </div>
-    );
+  const allProdsControl = useAnimation();
+
+  useEffect(() => {
+    const getProdsFormSer = () => {
+      var prodIds = getProdsFromLS();
+      axios
+        .post(`http://localhost:8000/api/basket/getByMass`, prodIds, axios.defaults.withCredentials = true)
+        .then(response => {
+          console.log(response.data);
+          parseProdsData(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    };
+    getProdsFormSer();
+  }, [isDeleteProd]);
+
+  const parseProdsData = data => {
+    if (Array.isArray(data)) {
+      const newData = data.map((item, index) => ({
+        uniqueKey: `${item.id}-${index}`,
+        id: item.id,
+        title: item.title,
+        description: item.description,
+        weight: item.weight,
+        price: item.price,
+        category: item.category,
+        diameter: item.diameter,
+        imgLink: item.imgLink,
+      }));
+      setProdData(newData);
+      if (newData.length > 0) {
+        setElementsVisible(true);
+      }
+    } else {
+      console.error('Полученные данные не являются массивом');
+    }
+  };
+
+  const deleteProd = uniqueKey => {
+    const itemToDelete = prodData.find(product => product.uniqueKey === uniqueKey);
+    delProdFromBasket(itemToDelete.id);
+    setProdData(prevData => {
+      const newData = prevData.filter(product => product.uniqueKey !== uniqueKey);
+      if (newData.length === 0 && !initialLoad.current) {
+        setElementsVisible(false);
+      }
+      return newData;
+    });
+  };
+
+  useEffect(() => {
+    if (initialLoad.current) {
+      initialLoad.current = false;
+    } else if (prodData.length === 0) {
+      setElementsVisible(false);
+    }
+  }, [prodData]);
+
+  async function resetBasket()
+  {
+    await allProdsControl.start({
+      x: 1900,
+      height: 0,
+      transition: { duration: 0.5 },
+    })
+    clearBasket()
+    setIsDeleteProd(!isDeleteProd)
+  }
+
+  return (
+    <div id='Basket' style={{ backgroundColor: theme.palette.background.paper }}>
+      <div id="basket">
+      <motion.div id='basketProducts' animate={allProdsControl}>
+        {prodData.map(product => (
+          <BasketProduct
+            key={product.uniqueKey}
+            id={product.id}
+            uniqueKey={product.uniqueKey}
+            theme={theme}
+            prdtDiameter={product.diameter}
+            prdtTitle={product.title}
+            prdtDescription={product.description}
+            prdtWeight={product.weight}
+            prdtPrice={product.price}
+            prdtCategory={product.category}
+            imgLink={product.imgLink}
+            deleteFunc={deleteProd}
+          />
+        ))}
+        </motion.div>
+        <AnimatePresence>
+          {!elementsVisible && !initialLoad.current && (
+            <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, transition: { duration: 1 } }}
+            id='emptyBasket'
+            >
+            <h3 style={{ color: theme.palette.text.primary }}>Корзина пуста</h3>
+            <Button href="/catalog" variant='contained'>В каталог</Button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <AnimatePresence>
+          {elementsVisible && (
+            <motion.div
+              id="buttonsBas"
+              initial={{ opacity: 1 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0, padding:0, transition: { duration: 0.5 } }}
+            >
+              <Button variant='contained' onClick={resetBasket}>Очистить корзину</Button>
+              <Button variant='contained'>Заказать</Button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </div>
+  );
 };
 
 export default Basket;
