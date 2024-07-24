7060797833cf70137ae840ba2318494fd946a694
Uvoi <iovu_dmtr@yahoo.com>
2024-07-24 17:13:00 +0300

    refactor(components/ProductCard) Убран ненужный пропс, изменение вызова функции добавления в корзину

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,21 +2,20 @@ import React, { useContext } from 'react';
 import './styles.css'
 import { useNavigate} from 'react-router-dom';
 
-import pass from '../../images/pass.jpg'
 import { Button } from '@mui/material';
 import basket from './../../images/basket.svg'
-import {addToBasket} from '../../functions/user/user'
+import {addToBasket} from '../../functions/basket'
 import { themeContext } from '../../App';
 
 
-const ProductCard = ({prdtImg="",cardId, prdtTitle, prdtDescription, prdtWeight, prdtCategory="", prdtPrice, imgLink=""})=>
+const ProductCard = ({cardId, prdtTitle, prdtDescription, prdtWeight, prdtCategory="", prdtPrice, imgLink=""})=>
 {
     let navigate = useNavigate();
     const theme = useContext(themeContext)
     return( 
         <div id={'ProductCard_'+cardId} className='ProductCard' style={{color: theme.palette.text.secondary}}>
             <div className="prdtImgData">
-                <img src={imgLink?imgLink:pass} onClick={()=>{navigate('/asteroid?id='+cardId)}} alt="" />
+                <img src={imgLink} onClick={()=>{navigate('/asteroid?id='+cardId)}} alt="" />
                 <span style={{color: theme.palette.text.primary}}>{prdtCategory}</span>
             </div>
             <div className="prdtData">
@@ -32,7 +31,7 @@ const ProductCard = ({prdtImg="",cardId, prdtTitle, prdtDescription, prdtWeight,
                         backgroundColor: theme.palette.primary.main, 
                         color:theme.palette.text.primary,
                         }}
-                        onClick={()=>{addToBasket(cardId)}}
+                        onClick={async()=>{await addToBasket(cardId)}}
                         >
                         <img src={basket} alt="" style={{filter:`invert(100%)`}}/>
                     </Button>
