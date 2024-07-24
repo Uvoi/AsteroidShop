578c1f92f8be0a0ee736778cbed4b1cefd8442de
Uvoi <iovu_dmtr@yahoo.com>
2024-07-24 17:42:35 +0300

    feat(functions/user) Добавлены функци обращения к серверу для смены полного имени и для смены адреса

--- src/functions/user.js
+++ src/functions/user.js
@@ -10,3 +10,31 @@ export async function checkSession() {
     }
 }
 
+export function changeFullName(firstName, lastName)
+{
+    const newName = {
+        'firstName': firstName,
+        'lastname': lastName
+    }
+    axios.patch(`http://localhost:8000/api/user/name`, {newName}, { withCredentials: true })
+    .then(response => {
+        console.log("Name changed");
+    })
+    .catch(error => {
+        console.log('Error change name: ', error);
+    });
+}
+
+export function changeAddress(address)
+{
+    const newAddress = {
+        'address': address
+    }
+    axios.patch(`http://localhost:8000/api/user/address`, {newAddress}, { withCredentials: true })
+    .then(response => {
+        console.log("Address changed");
+    })
+    .catch(error => {
+        console.log('Error change address: ', error);
+    });
+}
