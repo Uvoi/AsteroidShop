9d5717839968d01dd8a81bde49ca195ff0536763
Uvoi <iovu_dmtr@yahoo.com>
2024-07-31 12:12:34 +0300

    feat(components/OrderProduct) Добавление параметра отображения цены и возможности получения ссылки на картинку товара, если она не передается

--- src/components/OrderProduct/OrderProduct.jsx
+++ src/components/OrderProduct/OrderProduct.jsx
@@ -1,13 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './styles.css';
 import { themeContext } from '../../App';
+import axios from 'axios';
 
-const OrderProduct = ({ id, imgLink, price}) => {
+const OrderProduct = ({ id, imgLink, price, nonPrice=false}) => {
+    const [gettedImgLink, setGettedImgLink] = useState()
     const theme = useContext(themeContext)
+
+    useEffect(() => {
+        const getProdFormSer = () => {
+          axios
+            .get(`http://localhost:8000/api/products/`+id, axios.defaults.withCredentials = true)
+            .then(response => {
+                setGettedImgLink(response.data.imgLink)
+            })
+            .catch(error => {
+              console.error(error);
+            });
+        };
+
+        !imgLink && getProdFormSer();
+      }, []);
+
+
     return (
         <a style={{ color: theme.palette.text.primary }} className='OrderProduct' href={'/asteroid?id=' + id}>
-            <img src={imgLink} alt="asteroid" />
-            <p>{price}.000 <sub>₽</sub></p>
+            <img src={imgLink?imgLink:gettedImgLink} alt="asteroid" />
+            {!nonPrice &&
+                <p>{price}.000 <sub>₽</sub></p>
+            }
         </a>
     );
 };
