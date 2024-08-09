191c3c2a7bd62c0dda77ea29cb9e9a262f9d4eef
Uvoi <iovu_dmtr@yahoo.com>
2024-08-09 20:39:31 +0300

    refactor(components/Header) Обновление logo, добавление прокрутки по нажатию, добавлено возможность отображения пользовательского фото, svg корзины больше не обернуто в img, добавлено изменение цвета иконки корзины при наведении

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React, { useContext, useState } from 'react';
 import './styles.css'
-import logo from './../../images/logo.svg'
+import logo from './../../images/logo.png'
 import userLogo from './../../images/userImg.png'
 import basket from './../../images/basket.svg'
+import { ReactComponent as BasketSvg } from './../../images/basket.svg'
 import StarButton from '../StarButton/StarButton';
 import { Backdrop, Modal} from '@mui/material';
 import RegLogM from '../RegLogM/RegLogM';
-import axios from 'axios';
 import { themeContext, userContext } from '../../App';
 
 
@@ -17,29 +17,37 @@ const Header = ({onToggleTheme, updateUser, openRegLogModal, setOpenRegLogModal}
     const theme = useContext(themeContext)
     const user = useContext(userContext)
     const [regOrLog, setRegOrLog] = useState(true);
+    const [rotate, setRotate] = useState(false);
 
     const handleOpen = () => setOpenRegLogModal(true);
     const handleClose = () => setOpenRegLogModal(false);
 
+    const handleLogoClick = () =>
+        {
+            onToggleTheme()
+            setRotate(!rotate)
+        }
+
     return(
         <div id='Header' style={{background: theme.palette.header.primary}}>
             <div id="header">
-                <div id="LogoH"><button onClick={onToggleTheme}><img src={logo} alt="" /></button></div>
+                <div id="LogoH"><button onClick={handleLogoClick}><img className={rotate?'rotate':'unrotate'} src={logo} alt="" /></button></div>
                 <div id='MenuH' className='text_stroke'>
                     <ul>
                         <li ><StarButton href="/">Главная</StarButton></li>
                         <li ><StarButton href="/catalog">Каталог</StarButton></li>
                         <li ><StarButton href="/about">О нас</StarButton></li>
                         <li ><StarButton href="/help">Помощь</StarButton></li>
-                        
                     </ul>
                 </div>
                 <div id="Basket_UserDataH">
-                    <a id='BasketH' href="/basket"><img src={basket} alt="" /></a>
+                    <a id='BasketH' href="/basket">
+                        <BasketSvg/>
+                    </a>
                     {user!=null?
                     <div id="UserDataH">
                         <a href='/profile' className='bot_line'>{user.firstname}</a>
-                        <a href="/profile"><img src={userLogo} alt="" /></a>
+                        <a href="/profile"><img src={user.photo?user.photo:userLogo} alt="" /></a>
                     </div>
                     : 
                     <div id="UserLogReg">
