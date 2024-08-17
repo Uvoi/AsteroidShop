06c79749463d72a319570137b186dcd4f808f95e
Uvoi <iovu_dmtr@yahoo.com>
2024-08-17 16:11:44 +0300

    feat(functions/order) Создание функций для обращения к серверу для получения заказов пользователя и создания новых

---
+++ src/functions/order.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export async function getOrders() {
+    try {
+        const response = await axios.get('http://localhost:8000/api/order/', { withCredentials: true });
+        return response.data.orders || [];
+    } catch (error) {
+        console.log('Error fetching orders: ', error);
+        return [];
+    }
+}
+
+export async function addOrder(productIds, deliveryAddress) {
+    try {
+        const response = await axios.post('http://localhost:8000/api/order/add', {
+            productids: productIds,
+            deliveryaddress: deliveryAddress
+        }, { withCredentials: true });
+
+        return response.data;
+    } catch (error) {
+        console.error('Error adding order: ', error);
+        throw error;
+    }
+}
