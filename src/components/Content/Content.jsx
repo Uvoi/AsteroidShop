e331bccb3ca216cbf7fb27334ae595762a914b4c
Uvoi <iovu_dmtr@yahoo.com>
2024-08-29 00:28:50 +0300

    refactor(components/Content) Замена импортов большинства компонентов на lazy для оптимизации

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,37 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import './styles.css';
 
 import Home from '../../pages/Home/Home';
-import Catalog from '../../pages/Catalog/Catalog';
-import Help from '../../pages/Help/Help';
-import AddToDB from '../AddToDB/AddToDB';
 import Asteroid from '../../pages/Asteroid/Asteroid';
-import Basket from '../../pages/Basket/Basket';
-import Profile from '../../pages/Profile/Profile';
-import Order from '../../pages/Order/Order';
+const Catalog = lazy(() => import('../../pages/Catalog/Catalog'))
+const Help = lazy(() => import('../../pages/Help/Help'))
+const AddToDB = lazy(() => import('../../components/AddToDB/AddToDB'))
+const Basket = lazy(() => import('../../pages/Basket/Basket')) 
+const Profile = lazy(() => import('../../pages/Profile/Profile'))
+const Order = lazy(() => import('../../pages/Order/Order'))
+const AboutUs = lazy(() => import('../../pages/AboutUs/AboutUs'))
+const Unlogined = lazy(() => import('../../pages/Unlogined/Unlogined'))
 
 
-const Content = ({updateUser})=>
-{
+const Content = ({ updateUser }) => {
 
-    
-    return(
-        <div id='Main'>
-            <Router>
-                <Routes>
-                    <Route exact path='/' element={<Home/>}/>
-                    <Route exact path='/catalog' element={<Catalog/>}/>
-                    <Route exact path='/help' element={<Help/>}/>
-                    <Route exact path='/catalog/add' element={<AddToDB/>}/>
-                    <Route exact path='/asteroid' element={<Asteroid/>}/>
-                    <Route exact path='/basket' element={<Basket/>}/>
-                    <Route exact path='/basket/order' element={<Order/>}/>
-                    <Route exact path='/profile' element={<Profile updateUser={updateUser}/>}/>
-                </Routes>
-            </Router>
-        </div>
-    );
-};
+return (
+    <div id='Main'>
+        <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/catalog' element={<Catalog />} />
+            <Route exact path='/help' element={<Help />} />
+            <Route exact path='/about' element={<AboutUs />} />
+            <Route exact path='/catalog/add' element={<AddToDB />} />
+            <Route exact path='/asteroid' element={<Asteroid />} />
+            <Route exact path='/basket' element={<Basket />} />
+            <Route exact path='/basket/order' element={(
+                  <Order/>
+            )} />
+            <Route exact path='/profile' element={
+                    <Profile updateUser={updateUser} />
+            } />
+        </Routes>
+    </div>
+);
+}
 
 export default Content;
