0cd1add157e4d28deeb2c7cf2f2dc7c5fcf0a8e6
Uvoi <iovu_dmtr@yahoo.com>
2024-09-13 17:15:35 +0300

    feat(components/Comment) Добавление возможности нажатия на пользователя для просмотра его профиля

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -3,18 +3,21 @@ import './styles.css'
 
 import userPic from './../../images/userImg.webp'
 import { themeContext } from '../../App';
+import { Link } from 'react-router-dom';
 
 
-const Comment = ({user, dateTime, photo, children})=>
+const Comment = ({user, customerId, dateTime, photo, children})=>
 {
     const theme = useContext(themeContext)
     return(
         <div id='Comment'>
             <div id='comment'>
                 <div id="picAndUsernameCom" style={{color: theme.palette.text.ultra}}>
-                    <img src={photo?photo:userPic} alt="" />
-                    {user}
-                    </div>
+                    <Link to={'/user?id='+customerId}>
+                        <img src={photo?photo:userPic} alt="" />
+                        {user}
+                    </Link>
+                </div>
                 <div id="dataCom">
                     <div id="textCom" style={{color: theme.palette.text.primary, borderLeft: "1px solid"+theme.palette.text.secondary}}>{children}</div>
                     <div id="dateTimeCom" style={{color: theme.palette.text.secondary}}>{dateTime}</div>
